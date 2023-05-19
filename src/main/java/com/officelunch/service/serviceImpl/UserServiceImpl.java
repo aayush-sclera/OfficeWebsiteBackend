70cdd4f2b82052be2a76a4f676c501575c77ad54
Dinesh-Pariyar <dineshpariiyar1013@gmail.com>
2023-05-19 13:05:35 +0545

    added simple security with session

--- src/main/java/com/officelunch/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/officelunch/service/serviceImpl/UserServiceImpl.java
@@ -17,34 +17,41 @@ public class UserServiceImpl implements UserService {
     AvailabilityRepo availabilityRepo;
     private UserRepositories userRepositories;
     private BCryptPasswordEncoder encoder;
+
     @Autowired
-    UserServiceImpl(UserRepositories userRepositories, BCryptPasswordEncoder encoder){
+    UserServiceImpl(UserRepositories userRepositories, BCryptPasswordEncoder encoder) {
         this.userRepositories = userRepositories;
         this.encoder = encoder;
     }
 
     @Override
-    public void saveUser(User user) {
-        String password = encoder.encode(user.getPassword());
-        user.setPassword(password);
-        userRepositories.save(user);
-        Availability availability = new Availability();
-        availability.setId(user.getId());
-        availability.setUsername(user.getUsername());
-        availability.setUser(user);
-        availability.setFoodPref("Not Selected");
-        availabilityRepo.save(availability);
+    public String saveUser(User user) {
+        if (userRepositories.existsByUsername(user.getUsername())) {
+            return null;
+        } else {
+            String password = encoder.encode(user.getPassword());
+            user.setPassword(password);
+            userRepositories.save(user);
+            Availability availability = new Availability();
+            availability.setId(user.getId());
+            availability.setUsername(user.getUsername());
+            availability.setUser(user);
+            availability.setFoodPref("Not Selected");
+            availabilityRepo.save(availability);
+
+            return "registered Success";
+        }
     }
 
     @Override
     public User getUserByUserName(String username, String password) {
         User user = userRepositories.findByUsername(username);
         System.out.println(user.getPassword());
-        if(user == null){
+        if (user == null) {
             System.out.println("Invalid Username and Password");
             return null;
-        }else{
-            if(encoder.matches(password,user.getPassword())){
+        } else {
+            if (encoder.matches(password, user.getPassword())) {
                 return user;
             }
             return null;
@@ -54,7 +61,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User resetUserPassword(User user) {
         String password = encoder.encode(user.getPassword());
-        User usr=userRepositories.findByUsername(user.getUsername());
+        User usr = userRepositories.findByUsername(user.getUsername());
         usr.setPassword(password);
         return userRepositories.save(usr);
 
